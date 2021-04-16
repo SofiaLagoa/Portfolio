f0481b222afadbe5fc683fa179d552ef5a41d53a
Sofía Lagoa <lagoasofiaconstanza@gmail.com>
2021-04-16 17:19:29 -0300

    send email

--- enviar.php
+++ enviar.php
@@ -1,23 +1,16 @@
 <?php
+//get data from form  
 $name = $_POST['full-name'];
-$mail = $_POST['email'];
-//$phone = $_POST['phone'];
-$message = $_POST['message'];
-
-$header = 'From: ' . $mail . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
-
-$message = "Este mensaje fue enviado por: " . $name . " \r\n";
-$message .= "Su e-mail es: " . $mail . " \r\n";
-//$message .= "Teléfono de contacto: " . $phone . " \r\n";
-$message .= "Mensaje: " . $_POST['message'] . " \r\n";
-$message .= "Enviado el: " . date('d/m/Y', time());
-
-$para = 'lagoasofiaconstanza@gmail.com';
-$asunto = 'Mensaje de porfolio';
-
-mail($para, $asunto, utf8_decode($message), $header);
-
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "lagoasofiaconstanza@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: lagoasofiaconstanza@gmail.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:.html");
 ?>
