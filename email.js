0b271e646467e0140c854a232a4a3329aa76e6ae
Sofía Lagoa <lagoasofiaconstanza@gmail.com>
2021-04-16 19:22:56 -0300

    send email.js new

--- email.js
+++ email.js
@@ -1,15 +1,17 @@
-var templateParams = {
-    subject: "Email desde portfolio",
-    fullName: document.querySelector("#full-name").innerHTML,
-    email: document.querySelector("#email").innerHTML,
-    message: document.querySelector("message").innerHTML
-};
+(function() {
+    emailjs.init('user_3SpPKuE3XgqUTWiuseY7R');
+})();
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
 
-function sendEmail() {
-    emailjs.send('service_71836jh', 'template_o98pmw4', templateParams)
-        .then(function(response) {
-            console.log('SUCCESS!', response.status, response.text);
-        }, function(error) {
-            console.log('FAILED...', error);
-        });
+        // these IDs from the previous steps
+        emailjs.sendForm('service_71836jh', 'template_o98pmw4', this)
+            .then(function() {
+                window.location.href = "https://sofialagoa.github.io/portfolio/sobreMi.html"
+            }, function(error) {
+                console.log('No se pudo enviar el Email', error);
+            });
+    });
 }
